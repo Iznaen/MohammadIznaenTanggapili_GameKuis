3a79b234b456b453dcf90bc64701ddde0d3f7d48
Iznaen <tanggapili.iznaen@gmail.com>
2023-09-14 10:59:33 +0800

    Setup Repository
    Setup project repository

---
+++ Assets/main.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!");
+        Debug.Log("Game Starting...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*  to declare a variable define its 'type' and 'name':
+                > dataType varName
+                > int damage
+            data type in C# includes:
+                > int for integers
+                > char for single character
+                > string for characters sequence
+                > float for floating point
+                > double
+                > bool for boolean
+            examples:
+                > int damage = 10;
+                > float shield = 3.14f;
+                > double health = 22.5;
+                > char initial = 'A';
+                > string name = "Jack Schneider";
+                > bool on_fire = true;
+                 */        
+    }
+}
