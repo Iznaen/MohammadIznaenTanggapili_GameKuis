6b9a5bd5cbf950058ecd73b0554c56737d34c880
Iznaen <tanggapili.iznaen@gmail.com>
2023-09-16 16:53:31 +0800

    Core Gameplay
    Implementasi Core Gameplay:
    1. UI
    2. Image/Gambar
    3. Soal
    4. Pilihan Jawaban
    5. Pop-up saat jawaban dipilih benar/salah
    6. Pop-up tombol lanjut/ulang
    7. Pop-up waktu habis

---
+++ Assets/UI_Timer.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Timer : MonoBehaviour
+{
+    [SerializeField]
+    private UI_PesanLevel _tempatPesan = null;    
+
+    [SerializeField]
+    private Slider _timeBar = null;
+
+    [SerializeField]
+    private float _waktuJawab = 10f;
+
+    private float _sisaWaktu = 0f;
+    private bool _waktuBerjalan = true;
+
+    public bool WaktuBerjalan
+    {
+        get => _waktuBerjalan;
+        set => _waktuBerjalan = value;
+    }
+
+    private void Start()
+    {
+        UlangWaktu();
+    }
+
+    private void Update()
+    {
+        if (!_waktuBerjalan)
+            return;
+
+        _sisaWaktu -= Time.deltaTime;
+        _timeBar.value = _sisaWaktu / _waktuJawab;
+
+        if (_sisaWaktu <= 0f)
+        {
+            _tempatPesan.Pesan = "Waktu sudah habis!";
+            _tempatPesan.gameObject.SetActive(true);
+            //#Debug.Log("Waktu habis!");
+            _waktuBerjalan = false;
+            return;
+        }
+
+        //#Debug.Log(_sisaWaktu);
+    }
+
+    public void UlangWaktu()
+    {
+        _sisaWaktu = _waktuJawab;
+    }
+}
